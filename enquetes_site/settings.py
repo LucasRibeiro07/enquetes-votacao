2a01bda819dfc53f8278eaee9c2fa138ea44539b
Lucas Ribeiro <lucasdsrib@gmail.com>
2026-02-28 16:03:20 -0300

    Preparação para deploy no Render - Enquetes votação

--- enquetes_site/settings.py
+++ enquetes_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h5bdjmt&ambc-7bkkg=61npfg^%idpg39xw28^8eu-2$jiqgii
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # logo após SecurityMiddlewar
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,4 +116,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
