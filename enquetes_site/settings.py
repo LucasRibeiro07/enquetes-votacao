c42e9cd535d436ede8ac8d583e7a2a625a6346f2
Lucas Ribeiro <lucasdsrib@gmail.com>
2026-02-28 21:53:30 -0300

    Corrige 502 Bad Gateway: bind em  e ALLOWED_HOSTS dinâmico

--- enquetes_site/settings.py
+++ enquetes_site/settings.py
@@ -24,12 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h5bdjmt&ambc-7bkkg=61npfg^%idpg39xw28^8eu-2$jiqgii'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ  # True local, False no Render
+
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+ALLOWED_HOSTS.append('*')  # temporário para teste - remova depois
 
 # Application definition
 
